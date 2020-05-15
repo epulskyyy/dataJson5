6f9c50a38caaab358f86613fff9e5f1d609cbf36
Epul <51163683+epulskyyy@users.noreply.github.com>
2020-05-15 09:34:56 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,21 @@
-aasd
+{
+  "thisMonth": {
+    "status": "OK",
+    "invoice_bulan_berjalan": 1050,
+    "invoice_bulan_lalu": 150,
+    "data": [
+        {
+            "date": "2020-05-04",
+            "nominal": 150
+        },
+        {
+            "date": "2020-05-05",
+            "nominal": 0
+        },
+        {
+            "date": "2020-05-06",
+            "nominal": 900
+        }
+    ]
+  }
+}
